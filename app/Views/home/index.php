a520cbef07552544355b8ee18def9423816ce89d
Your Name <you@example.com>
2024-06-12 15:34:03 -0300

    pika

---
+++ app/Views/home/index.php
@@ -0,0 +1,26 @@
+<div class="text-bg-secondary p-3">
+    <img src="https://sites.uel.br/bibliotecas/wp-content/uploads/2024/04/bv-digital.png"  class="sticky-xxl-top" > 
+    <h2>Sejam Bem-vindos(as) à nossa Biblioteca Virtual </h2>
+    <br>
+    <div class="row p-7">
+        <div class="col-5">
+            <p>
+            </p>
+            <p>
+                Bem-vindo à nossa biblioteca virtual, um espaço onde as páginas se tornam portais para aventuras, 
+                conhecimento e imaginação. Aqui, você não precisa se preocupar com horários de funcionamento, 
+                pois as portas estão sempre abertas, prontas para recebê-lo a qualquer momento do dia ou da noite.
+            </p>
+            <p>
+            Ao adentrar este mundo de letras e ideias, convidamo-lo a explorar os vastos corredores da literatura,
+             onde cada livro é uma janela para um novo universo. De clássicos intemporais a contemporâneos vibrantes,
+              há algo aqui para cada tipo de leitor, seja você um ávido devorador de romances,
+             um curioso buscador de conhecimento ou um explorador de mundos distantes.
+            </p>
+            <p>
+            Seja bem-vindo à nossa biblioteca virtual. Onde a magia das palavras espera por você a cada clique.
+
+            </p>
+        </div>
+    </div>
+</div>   
